f3691d999e7b907373738ef4d11a3b4d0f62a8eb
Kent C. Dodds <kent+github@doddsfamily.us>
2015-01-05 12:53:19 -0700

    Adding sourcemaps to echo.js and fixing a silly bug

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     library: 'Echo',
     libraryTarget: 'umd'
   },
+  devtool: 'inline-source-map',
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: '6to5-loader'}
